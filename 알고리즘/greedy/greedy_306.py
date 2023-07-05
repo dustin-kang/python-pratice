26680948d3dabd64791f9aa4da8963499376d4a2
dustin-kang <dongwoomail@icloud.com>
2023-07-05 15:36:11 +0900

    그리디

---
+++ 알고리즘/greedy/greedy_306.py
@@ -0,0 +1,33 @@
+"""
+무지의 먹방 라이브
+https://school.programmers.co.kr/learn/courses/30/lessons/42891
+"""
+
+from collections import deque
+def solution(food_times, k):
+    """
+    1 ~ N 까지 음식을 ㅌㄴ섭취
+    1초 동안 섭취 후 남은 음식 큐를 통해 뒤로 보내기
+    K 때 까지 먹고난 후 다음 음식 번호 
+    
+    - 각 음식 당 시간 foodtimes
+    - 장애 발생시간 K
+    """
+    answer = 1
+    food_times = deque(food_times)
+    length = len(food_times)
+    while k>0:
+        food = food_times.popleft()
+        answer = (answer + 1) % length
+        if food != 0:
+            food -= 1
+            food_times.append(food)
+            k -= 1
+        else:
+            food_times.append(food)
+            continue
+
+    return answer
+
+if __name__ == "__main__":
+    print(solution([3, 1, 2], 5))
