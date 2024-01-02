060086c899d85a8aeaf296b5972d11ff4cad0b1e
dustin-kang <dongwoomail@icloud.com>
2024-01-02 12:00:27 +0900

    코테 스터디 240102

--- 알고리즘/이코테/309.py
+++ 알고리즘/이코테/309.py
@@ -4,23 +4,30 @@
 https://school.programmers.co.kr/learn/courses/30/lessons/60057
 """
 
+"""
+만약 문자열이 8개라면 최대 4개단위까지 끊을 수 있다.
+이유는 5개가 되면 최소 문자열이 10개가 필요하기 때문
+
+만약 해당 문자열과 바로 뒤 문자열이 동일할 경우 count를 올려 새로운 문자열에 붙인다.
+틀리다면, 해당 문자열 그대로 붙이고 다음 문자열로 이동한다.
+
+그리고 마지막에 저장된 최종 문자열과 새로 들어온 문자열의 길이를 비교한다.
+"""
 def solution(s):
     answer = len(s)
-    for step in range(1, len(s) +1): # 1개 2개 3개 ... 단위로 자르기
-        compressed = ""
+    for step in range(1, len(s) // 2+1):
+        temp_str = ""
         prev = s[0:step]
-        count = 1 # 문자앞에 붙일 숫자
-        for j in range(step, len(s), step):
-            # prev와 같은 문자열이라면
-            if prev == s[j:j + step]:
+        count = 1
+        for i in range(step, len(s), step):
+            if prev == s[i:i+step]:
                 count += 1
-            # 다른 문자열일 경우(압축이 불가능)
             else:
-                compressed += str(count) + prev if count >= 2 else prev
-                prev = s[j:j + step] # 상태 초기화 다음 문자열
+                temp_str += str(count) + prev if count >= 2 else prev
+                prev = s[i:i+step]
                 count = 1
-        compressed += str(count) + prev if count >= 2 else prev
-        answer = min(answer, len(compressed)) # 갱신
+        temp_str += str(count) + prev if count >= 2 else prev
+        answer = min(answer, len(temp_str))
     return answer
 
 
